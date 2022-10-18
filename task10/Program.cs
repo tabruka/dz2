06756386b488f6ecee8468e7ff943a20ce913605
Aleksandr <tabruka@yandex.ru>
2022-10-18 12:31:42 +0300

    Добавил задачу №10

---
+++ task10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine());
+
+int FindSecondDigit(int number)
+{
+    int num3 = number / 10;
+    int num2 = num3 % 10;
+    return num2;
+}
+Console.WriteLine($"Вторая цифра числа {number} = {FindSecondDigit(number)}");
