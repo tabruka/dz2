ae9e02a0474540adbeaf1b9fe13df6620e900c42
Aleksandr <tabruka@yandex.ru>
2022-10-19 10:44:46 +0300

    Добавил задачу №9

---
+++ task9/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// int result = 7 * 23;
+// Console.WriteLine("Введите число");
+// int number = int.Parse(Console.ReadLine());
+// if(number == result)
+// {
+//     result = number;
+//     Console.WriteLine($"Число {number} кратно 7 и 23");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {number} не кратно 7 и 23");
+// }
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+if (number % 7 == 0  && number % 23 == 0)
+{
+    Console.WriteLine($"Число {number} кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {number} не кратно 7 и 23");
+}
