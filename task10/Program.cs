ee8d71474490741b1e3cb141d7f33e00e01577ac
Aleksandr <tabruka@yandex.ru>
2022-10-19 10:52:11 +0300

    Добавил задачу №10

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите трехзначное число");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число");
 int number = int.Parse(Console.ReadLine());
 
 int FindSecondDigit(int number)
